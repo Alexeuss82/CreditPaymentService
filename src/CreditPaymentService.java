97cdb5505e390dc08046eb2bf3858496b4d89f13
Alexeuss82 <krukovalex@gmail.com>
2022-11-06 12:00:52 +0200

    correct variables name

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public double calculate(double n, int y, double p) {
-        double r = p / 100 / 12; // месячная процентная ставка
-        double m = n * ((r * Math.pow((1 + r), y)) / (Math.pow((1 + r), y) - 1)); // сумма ежемесячного аннуитентного платежа
-        return m;
+    public double calculate(double loanSum, int creditTerm, double interestRate) {
+        double monthInterestRate = interestRate / 100 / 12; // месячная процентная ставка
+        double monthPaymentAmount = loanSum * ((monthInterestRate * Math.pow((1 + monthInterestRate), creditTerm)) / (Math.pow((1 + monthInterestRate), creditTerm) - 1)); // сумма ежемесячного аннуитентного платежа
+        return monthPaymentAmount;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int n = 1_000_000; //сумма кредита
-        int y = 12; //срок кредита
-        double p = 9.99; // годовая процентная ставка
-        double m = service.calculate(n, y, p); // сумма ежемесячного аннуитентного платежа
+        int loanSum = 1_000_000; //сумма кредита
+        int creditTerm = 12; //срок кредита
+        double interestRate = 9.99; // годовая процентная ставка
+        double monthPaymentAmount = service.calculate(loanSum, creditTerm, interestRate); // сумма ежемесячного аннуитентного платежа
 
         System.out.println();
-        System.out.println("Срок кредита 1 год. Платеж=" + service.calculate(1000000,12,9.99) + " руб. " + "Процентная ставка" + " 9.99%");
+        System.out.println("Срок кредита 1 год. Платеж = " + service.calculate(1000000, 12, 9.99) + " руб. " + "Процентная ставка" + " 9.99%");
 
         System.out.println();
-        System.out.println("Срок кредита 2 года. Платеж=" + service.calculate(1000000,24,9.99) + " руб. " + "Процентная ставка" + " 9.99%");
+        System.out.println("Срок кредита 2 года. Платеж = " + service.calculate(1000000, 24, 9.99) + " руб. " + "Процентная ставка" + " 9.99%");
 
         System.out.println();
-        System.out.println("Срок кредита 3 года. Платеж=" + service.calculate(1000000,36,9.99) + " руб. " + "Процентная ставка" + " 9.99%");
+        System.out.println("Срок кредита 3 года. Платеж = " + service.calculate(1000000, 36, 9.99) + " руб. " + "Процентная ставка" + " 9.99%");
     }
 }
